9ef51c0855f1330f3d09c220b3cb9f4685949a14
Edmond Hui <edmond.t.hui@gmail.com>
2020-11-16 17:41:44 -0500

    edit files for hosting on heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,8 @@
-import {createStore, applyMiddleware} from 'redux'
-import rootReducer from '../reducers/root_reducer'
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import thunk from '../middleware/thunk.js';
+// import logger from 'redux-logger'
 
-
-
-const configureStore = (preloadedState = {}) => (createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger)));
+const configureStore = (preloadedState = {}) => (createStore(rootReducer, preloadedState, applyMiddleware(thunk)));
 
 export default configureStore
