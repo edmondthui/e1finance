501854b8d99efe759ffa19c9b063da6cc0d83e94
Edmond Hui <edmond.t.hui@gmail.com>
2020-11-19 11:53:07 -0500

    change api keys to sandbox

--- frontend/util/IEX_api_utl.js
+++ frontend/util/IEX_api_utl.js
@@ -2,15 +2,16 @@ export const fetchCompanyNews = (ticker) => (
   $.ajax({
     method: 'GET',
     // url: `https://cloud.iexapis.com/stable/stock/${ticker.toLowerCase()}/news/last/10?token=${window.api_key}`
-    url: `https://sandbox.iexapis.com/stable/stock/${ticker.toLowerCase()}/news/last/10?token=${window.api_key}`
-    // COMMENTED OUT FOR NOW TO NOT WASTE API USES
+    url: `https://sandbox.iexapis.com/stable/stock/${ticker.toLowerCase()}/news/last/10?token=Tpk_b52b6d3dff234c59994a24ccc7ecd48f`
+    // COMMENTED OUT FOR NOW TO NOT WASTE API USES SANDBOX ^
    })
 );
 
 export const fetchInterdayData = (ticker) => (
   $.ajax({
     method: 'GET',
-    url: `https://cloud.iexapis.com/stable/stock/${ticker.toLowerCase()}/intraday-prices?token=${window.api_key}`,
-    // url: `https://sandbox.iexapis.com/stable/stock/${ticker.toLowerCase()}/intraday-prices?token=${window.api_key}`
+    // url: `https://cloud.iexapis.com/stable/stock/${ticker.toLowerCase()}/intraday-prices?token=${window.api_key}`,
+    url: `https://sandbox.iexapis.com/stable/stock/${ticker.toLowerCase()}/intraday-prices?token=Tpk_b52b6d3dff234c59994a24ccc7ecd48f`
+    // SANDBOX ^
   })
 )
