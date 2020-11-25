db8ed2cbf686d7f5c01ea9e9e3676c71db1b0dd2
Edmond Hui <edmond.t.hui@gmail.com>
2020-11-25 01:12:30 -0500

    :bug: fixed all bugs in chart

--- frontend/components/portfolio/chart.jsx
+++ frontend/components/portfolio/chart.jsx
@@ -12,34 +12,23 @@ class Chart extends React.Component {
             hover: false,
             chartX: null,
             render: false,
-            chart: null
+            chart: null,
         }
         this.data = [];
         this.formattedChart = [];
         this.formatData = this.formatData.bind(this)
+        this.formatDataArray = this.formatDataArray.bind(this)
     }
 
     componentDidMount() {
         this.props.fetchStocks()
         if (Array.isArray(this.props.tickers)) {
-            let holdingsLength = this.props.tickers.length
             this.props.tickers.forEach((ticker, idx)=> {
                 fetchInterdayData(ticker).then(response => {
                     // this would work a lot better if there was a fetch multiple ticker data api, this code is still buggy but theres nothing I can do.
                     // it works sometimes
                     this.data.push(response)
-                }).then(() => {
-                    if (idx === holdingsLength-1) {
-                        this.data.forEach((data, idx) => {
-                            let stocks = this.props.stocks
-                            let tickers = this.props.tickers
-                            let dataObj = {price: (data[data.length-1].high + data[data.length-1].low) / 2, id: stocks.filter(stock => stock.ticker === tickers[idx])[0].id}
-                            // this.props.updateStock(dataObj);
-                            // updating stock prices makes app glitch out
-                            // this.setState({render: true})
-                            this.formatData();
-                        })
-                    }
+                    this.formatDataArray(response, idx);
                 })
             })
         }
@@ -68,71 +57,51 @@ class Chart extends React.Component {
     }
 
     formatData() {
-        if (Array.isArray(this.props.quantities)) {
-            for (let i = 0 ; i < this.data[0].length ; i ++) {
-                let sumHigh = 0;
-                let label = "";
-                for (let j = 0 ; j<this.data.length; j++) {
-                    sumHigh += ((this.data[j][i].high + this.data[j][i].low) / 2) * this.props.quantities[j]
-                    label = this.data[j][i].label
-                }
-                this.formattedChart.push({high: sumHigh, label: label})
+        for (let i = 0 ; i < this.data[0].length ; i ++) {
+            let average = 0;
+            let label = "";
+            for (let j = 0 ; j<this.data.length; j++) {
+                average += ((this.data[j][i].high + this.data[j][i].low) / 2) * this.props.quantities
+                label = this.data[j][i].label
             }
-            debugger;
-        }
-        else {
-            for (let i = 0 ; i < this.data[0].length ; i ++) {
-                let sumHigh = 0;
-                let label = "";
-                for (let j = 0 ; j<this.data.length; j++) {
-                    sumHigh += ((this.data[j][i].high + this.data[j][i].low) / 2) * this.props.quantities
-                    label = this.data[j][i].label
-                }
-                this.formattedChart.push({high: sumHigh, label: label})
-            }
+            this.formattedChart.push({average: average, label: label})
         }
         this.setState({chart: this.formattedChart})
     }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.tickers.length !== this.props.tickers.length) {
-            fetchInterdayData(this.props.tickers).then(response => {
-                this.data.push(response)
-                let dataObj = {price: (response[response.length-1].high + response[response.length-1].low) / 2, id: this.props.id}
-                // this.props.updateStock(dataObj);
-                this.formatData();
-            })
+    formatDataArray(response, idx) {
+        let average = 0;
+        let label;
+        for (let i = 0 ; i<response.length; i++) {
+            average = ((response[i].high + response[i].low) / 2) * this.props.quantities[idx]
+            label = response[i].label
+            this.formattedChart[i] ? this.formattedChart[i].average += average : this.formattedChart.push({average: average, label: label})
         }
+        debugger;
+        this.setState({chart: this.formattedChart})
     }
 
+    // componentDidUpdate(prevProps) {
+    //     if (prevProps.tickers.length !== this.props.tickers.length) {
+    //         fetchInterdayData(this.props.tickers[this.props.tickers.length-1]).then(response => {
+    //             this.data.push(response)
+    //         })
+    //     }
+    // }
+
     render() {
-        // if (this.state.render) {
-        //     if (Array.isArray(this.props.quantities)) {
-        //         for (let i = 0 ; i < this.data[0].length ; i ++) {
-        //             let sumHigh = 0;
-        //             let label = "";
-        //             for (let j = 0 ; j<this.data.length; j++) {
-        //                 sumHigh += (this.data[j][i].high + this.data[j][i].low) / 2 * this.props.quantities[j]
-        //                 label = this.data[j][i].label
-        //             }
-        //             this.formattedChart.push({high: sumHigh, label: label})
-        //         }
-        //     }
-        //     else {
-        //         for (let i = 0 ; i < this.data[0].length ; i ++) {
-        //             let sumHigh = 0;
-        //             let label = "";
-        //             for (let j = 0 ; j<this.data.length; j++) {
-        //                 sumHigh += (this.data[j][i].high + this.data[j][i].low) / 2 * this.props.quantities
-        //                 label = this.data[j][i].label
-        //             }
-        //             this.formattedChart.push({high: sumHigh, label: label})
-        //         }
-        //     }
-        //     debugger;
-        //     this.setState({chart: this.formattedChart})
-        //     this.setState({render: false})
-        // }
+        let chart;
+        if (this.state.chart) {
+            chart = <ResponsiveContainer>
+                <LineChart height={300} data={this.state.chart} onMouseMove={(data) => this.updateState(data)} onMouseLeave={()=>this.setState({value: null, hover: false, chartX: null, label: null})}>
+                    <Line type="monotone" dataKey="average" stroke="#00D4A3" strokeWidth={2} fill="#8884d8" dot={false} />
+                    <XAxis dataKey="minute" hide={true} />
+                    <YAxis type="number" domain={['dataMin', 'dataMax']} hide={true} />
+                    <Tooltip contentStyle={{opacity: 0}} cursor={{strokeWidth: 20, opacity: .2}}/>
+                </LineChart>
+            </ResponsiveContainer>
+        }
+        debugger;
         return (
             <div className="chart-container">
                 <div className="portfolio-chart">
@@ -141,14 +110,7 @@ class Chart extends React.Component {
                         <p className="portfolio-graph-value">{this.state.value}</p>
                         <p className="portfolio-graph-label">{this.state.label}</p>
                     </div>
-                    <ResponsiveContainer>
-                        <LineChart height={300} data={this.state.chart} onMouseMove={(data) => this.updateState(data)} onMouseLeave={()=>this.setState({value: null, hover: false, chartX: null, label: null})}>
-                            <Line type="monotone" dataKey="high" stroke="#00D4A3" strokeWidth={2} fill="#8884d8" dot={false} />
-                            <XAxis dataKey="minute" hide={true} />
-                            <YAxis type="number" domain={['dataMin', 'dataMax']} hide={true} />
-                            <Tooltip contentStyle={{opacity: 0}} cursor={{strokeWidth: 20, opacity: .2}}/>
-                        </LineChart>
-                    </ResponsiveContainer>
+                    {chart}
                 </div>
             </div>
         )
